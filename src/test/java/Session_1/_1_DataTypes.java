094b16e9b40961419cf0d5603fb2abbfcfb30719
DalviMadhuri123 <madhuridalvi10@gmail.com>
2024-02-09 14:54:03 +0530

    initial proj setup

---
+++ src/test/java/Session_1/_1_DataTypes.java
@@ -0,0 +1,14 @@
+package Session_1;
+
+public class _1_DataTypes {
+	
+	public static void main(String[] args)
+	{
+		int height;
+		height = 10;
+		System.out.println(height);
+		
+		int ageOfUser = 19;
+		System.out.println(ageOfUser);
+	}
+}
